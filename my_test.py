57db1e88864f47dbfd06016d60276d3ee533ae2e
Vitor Hugo <vitorhugo02cc@gmail.com>
2025-05-01 22:19:20 -0300

    6. Classes TestLoader e TestRunner

--- my_test.py
+++ my_test.py
@@ -1,41 +1,9 @@
-from test_case import TestCase, TestCaseTest
-from test_result import TestResult
-from test_suite import TestSuite, TestSuiteTest
-
-class MyTest(TestCase):
-
-    def set_up(self):
-        print('set_up')
-
-    def tear_down(self):
-        print('tear_down')
-
-    def test_a(self):
-        print('test_a')
-
-    def test_b(self):
-        print('test_b')
-
-    def test_c(self):
-        print('test_c')
-
+from test_loader import TestLoader, TestLoaderTest
+from test_runner import TestRunner
 
 if __name__ == "__main__":
-    result = TestResult()
-    suite = TestSuite()
+    loader = TestLoader()
+    suite = loader.make_suite(TestLoaderTest)
 
-    suite.add_test(TestCaseTest('test_result_success_run'))
-    suite.add_test(TestCaseTest('test_result_failure_run'))
-    suite.add_test(TestCaseTest('test_result_error_run'))
-    suite.add_test(TestCaseTest('test_result_multiple_run'))
-    suite.add_test(TestCaseTest('test_was_set_up'))
-    suite.add_test(TestCaseTest('test_was_run'))
-    suite.add_test(TestCaseTest('test_was_tear_down'))
-    suite.add_test(TestCaseTest('test_template_method'))
-
-    suite.add_test(TestSuiteTest('test_suite_size'))
-    suite.add_test(TestSuiteTest('test_suite_success_run'))
-    suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
-    suite.run(result)
-    print(result.summary())
+    runner = TestRunner()
+    runner.run(suite)
---
+++ test_loader.py
@@ -0,0 +1,52 @@
+from test_suite import TestSuite
+from test_case import TestCase
+
+class TestLoader:
+
+    TEST_METHOD_PREFIX = 'test'
+
+    def get_test_case_names(self, test_case_class):
+        methods = dir(test_case_class)
+        test_method_names = list(filter(lambda method: 
+            method.startswith(self.TEST_METHOD_PREFIX), methods))
+        return test_method_names
+
+    def make_suite(self, test_case_class):
+        suite = TestSuite()
+        for test_method_name in self.get_test_case_names(test_case_class):
+            test_method = test_case_class(test_method_name)
+            suite.add_test(test_method)
+        return suite
+
+class TestLoaderTest(TestCase):
+
+    def test_create_suite(self):
+        loader = TestLoader()
+        suite = loader.make_suite(TestStub)
+        assert len(suite.tests) == 3
+
+    def test_create_suite_of_suites(self):
+        loader = TestLoader()
+        stub_suite = loader.make_suite(TestStub)
+        spy_suite = loader.make_suite(TestSpy)
+
+        suite = TestSuite()
+        suite.add_test(stub_suite)
+        suite.add_test(spy_suite)
+
+        assert len(suite.tests) == 2
+
+    def test_get_multiple_test_case_names(self):
+        loader = TestLoader()
+        names = loader.get_test_case_names(TestStub)
+        assert names == ['test_error', 'test_failure', 'test_success']
+
+    def test_get_no_test_case_names(self):
+
+        class Test(TestCase):
+            def foobar(self):
+                pass
+
+        loader = TestLoader()
+        names = loader.get_test_case_names(Test)
+        assert names == []
